1f6c34cb6e2b9935068512caf841d29f1ecf0b70
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-05-20 21:06:56 +0200

    Added db connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromptModule } from './prompt/prompt.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDBConnectionconfig } from './utils';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.MODE === 'local' ? 'host.docker.internal' : '127.0.0.1',
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      ssl: { rejectUnauthorized: false },
-    }),
+    TypeOrmModule.forRoot(getDBConnectionconfig()),
     PromptModule,
   ],
   controllers: [AppController],
---
+++ src/utils/db.utils.ts
@@ -0,0 +1,24 @@
+export const getDBConnectionconfig = (): any => {
+  if (process.env.MODE === 'prod') {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
+  }
+
+  return {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  };
+};
