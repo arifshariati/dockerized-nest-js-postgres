247405a02debf98fffacdb95ecc1f41c3f9e322b
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-05-20 17:20:09 +0200

    Added Swagger documentation and its related configurations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+const logger = new Logger('Backend');
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.enableCors();
+  // Swagger Configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Dockerized Nest JS API Gateway')
+    .setDescription(
+      'This API gatway documentation is for the dockerized nest js application with Postgresql DB',
+    )
+    .setVersion('v1.0.0')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, doc);
+
   const PORT = process.env.PORT || 4999;
-  await app.listen(PORT);
-}
+  await app.listen(PORT, () =>
+    logger.log(`Backend is listening on port ${PORT}`),
+  );
+};
+
 bootstrap();
