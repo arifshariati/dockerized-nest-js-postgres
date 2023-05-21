99317f02417907905e986dbdad3234fb44818dc8
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-05-21 11:26:52 +0200

    made user and prompt routes secure

--- src/prompt/prompt.controller.ts
+++ src/prompt/prompt.controller.ts
@@ -7,51 +7,69 @@ import {
   Param,
   Delete,
   UsePipes,
+  UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PromptService } from './prompt.service';
 import { CreatePromptDto } from './dto/create-prompt.dto';
 import { UpdatePromptDto } from './dto/update-prompt.dto';
-import { ApiForbiddenResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
 import { ValidationPipe } from 'src/pipes/validation.pipe';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('prompt')
 @Controller('prompt')
 export class PromptController {
   constructor(private readonly promptService: PromptService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @UsePipes(new ValidationPipe())
-  @Post()
   @ApiOkResponse({ description: 'The resource was created succesfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Post()
   create(@Body() createPromptDto: CreatePromptDto) {
     return this.promptService.create(createPromptDto);
   }
 
-  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resources were returned successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Get()
   findAll() {
     return this.promptService.findAll();
   }
 
-  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resource was returned successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.promptService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @UsePipes(new ValidationPipe())
-  @Patch(':id')
   @ApiOkResponse({ description: 'The resource was updated successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Patch(':id')
   update(@Param('id') id: string, @Body() updatePromptDto: UpdatePromptDto) {
     return this.promptService.update(id, updatePromptDto);
   }
 
-  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resource was deleted successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Delete(':id')
   remove(@Param('id') id: string) {
     return this.promptService.remove(id);
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,12 +7,19 @@ import {
   Delete,
   UsePipes,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ApiForbiddenResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { ILoginResponse } from './interfaces';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('user')
 @Controller('user')
@@ -28,30 +35,36 @@ export class UserController {
   }
 
   @UsePipes(new ValidationPipe())
-  @Post()
+  @Post('login')
   @ApiOkResponse({ description: 'The resource was found' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
   login(@Body() loginDto: LoginDto): Promise<ILoginResponse> {
     return this.userService.login(loginDto);
   }
 
-  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resources were returned successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resource was returned successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
-  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @ApiOkResponse({ description: 'The resource was deleted successfully' })
   @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
   }
